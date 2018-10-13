488b9e17ad831229de2dbc03082ddd6129bee47d
Christian Vögl <mail@2718-mail.de>
2018-10-13 11:36:12 +0200

    h

---
+++ Address.h
@@ -0,0 +1,11 @@
+ #pragma once
+ struct Address
+ {
+    std::string name;
+    std::string street;
+    std::string house;
+    std::string additionalInfo;
+    std::string PLZ;
+    std::string City;
+
+ }
